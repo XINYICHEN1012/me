bffc608acd6fe4a14aeb75fe651ef22cd1e4f19d
Xinyi Chen <shandy@novanb.com>
2020-07-29 08:12:30 +1000

    0729

--- week4/exercise1.py
+++ week4/exercise1.py
@@ -36,16 +36,19 @@ def get_some_details():
     json_data = open(LOCAL + "/lazyduck.json").read()
 
     data = json.loads(json_data)
-    return {"lastName": None, "password": None, "postcodePlusID": None}
+    him = data["results"][0]
+    return{
+        "lastName": him["name"]["last"], 
+        "password": him["login"]["password"], 
+        "postcodePlusID": int(him["location"]["postcode"]) + int(him["id"]["value"])
+        }
 
 
 def wordy_pyramid():
     """Make a pyramid out of real words.
-
     There is a random word generator here:
-    http://api.wordnik.com/v4/words.json/randomWords?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5&minLength=10&maxLength=10&limit=1
-    The arguments that the generator takes is the minLength and maxLength of the word
-    as well as the limit, which is the the number of words. 
+    https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength=20
+    The generator takes a single argument, length (`wordlength`) of the word.
     Visit the above link as an example.
     Use this and the requests library to make a word pyramid. The shortest
     words they have are 3 letters long and the longest are 20. The pyramid
@@ -72,9 +75,18 @@ def wordy_pyramid():
     "Nereis",
     "Leto",
     ]
-    TIP: to add an argument to a URL, use: ?argName=argVal e.g. &minLength=
+    TIP: to add an argument to a URL, use: ?argName=argVal e.g. &wordlength=
     """
-    pass
+    x = []
+    for i in range(3, 20, 2):
+        response = requests.get(f"https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength={i}")
+        print(response.text)
+        x.append(response.text)
+    for i in range(20, 3, -2):
+        response = requests.get(f"https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength={i}")
+        print(response.text)
+        x.append(response.text)
+    return x
 
 
 def pokedex(low=1, high=5):
@@ -93,11 +105,24 @@ def pokedex(low=1, high=5):
     """
     template = "https://pokeapi.co/api/v2/pokemon/{id}"
 
-    url = template.format(id=5)
-    r = requests.get(url)
-    if r.status_code is 200:
-        the_json = json.loads(r.text)
-    return {"name": None, "weight": None, "height": None}
+    tallest = 0
+    pokemon_list = []
+    for i in range(low, high):
+        url = template.format(id=i)
+        r = requests.get(url)
+        if r.status_code is 200:
+            the_json = json.loads(r.text)
+            pokemon_list.append(the_json)
+    
+    for a in pokemon_list:
+        height_new = a["height"]
+        if height_new > tallest:
+            tallest = height_new
+            name = a["name"]
+            weight = a["weight"] 
+            height = a["height"]
+    
+    return {"name": name, "weight": weight, "height": height}
 
 
 def diarist():
@@ -114,7 +139,16 @@ def diarist():
          the test will have nothing to look at.
     TIP: this might come in handy if you need to hack a 3d print file in the future.
     """
-    pass
+    file_r = (LOCAL + "/Trispokedovetiles(laser).gcode")
+    lasers = open(file_r, "r")
+    numcount = 0 
+    for line in lasers:
+        if "M10 P1" in line:
+            numcount += 1
+    
+    lasers = open(LOCAL + "/lasers.pew", "w")
+    lasers.write(str(numcount))
+    lasers.close()
 
 
 if __name__ == "__main__":
